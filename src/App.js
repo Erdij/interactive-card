07d63fd5fca327f072d34cd5e006fdc034466fde
Erdij <yildirimsukruerdi@gmail.com>
2023-01-12 17:47:00 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import bgMobile from "./images/bg-main-mobile.png";
+import bgDesktop from "./images/bg-main-desktop.png";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <section>
+        <div className="absolute -z-10">
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={bgDesktop} />
+            <img src={bgMobile} alt="" />
+          </picture>
+        </div>
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          <div>
+            <article className="front-card">Front Credit Card</article>
+            <article>Back Credit Card</article>
+          </div>
+          <div>form</div>
+        </div>
+      </section>
+    </>
+  );
 }
