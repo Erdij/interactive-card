1666e59ec50614ddc7a99a028d3b731c9f9e35a0
Erdij <yildirimsukruerdi@gmail.com>
2023-01-26 00:51:26 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import bgMobile from "./images/bg-main-mobile.png";
 import bgDesktop from "./images/bg-main-desktop.png";
-
+import logo from "./images/card-logo.svg";
 export default function App() {
   return (
     <>
@@ -13,9 +13,28 @@ export default function App() {
           </picture>
         </div>
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-          <div>
-            <article className="front-card">Front Credit Card</article>
-            <article>Back Credit Card</article>
+          <div className="mt-10 mx-5">
+            <article className="front-card p-5 flex flex-col justify-between ">
+              <img src={logo} alt="" className="w-20 lg:w-28" />
+              <div>
+                <h2 className="text-white text-xl lg:text-xl  mb-6 tracking-widest ">
+                  1234 5678 9012 3456
+                </h2>
+                <ul className="flex items-center justify-between">
+                  <li className="text-white  uppercase text-base lg:text-xl tracking-widest">
+                    Buraya Bakarlar
+                  </li>
+                  <li className="text-white  lg:text-xl tracking-widest">
+                    01/10
+                  </li>
+                </ul>
+              </div>
+            </article>
+            <article className="back-card relative">
+              <p className="absolute right-10  text-lg lg:text-xl text-white tracking-widest ">
+                123
+              </p>
+            </article>
           </div>
           <div>form</div>
         </div>
